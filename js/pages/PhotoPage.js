f51baed2cf9afa79875c3d9ff651ef1b77271727
li.zhaoguang <li.zhaoguang@immomo.com>
2018-01-29 17:17:26 +0800

    基本完成 PhotoPage

--- js/pages/PhotoPage.js
+++ js/pages/PhotoPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import {StatusBar, Text, View, Animated, BackHandler} from "react-native";
+import {StatusBar, Text, View, Animated, BackHandler, Easing, Dimensions} from "react-native";
 import navOption from "../components/NavOption";
 import {colors} from "../Styles";
 import TPTouchable from "../components/TPTouchable";
 import PropTypes from 'prop-types';
 import RootSiblings from "react-native-root-siblings";
+import FastImage from "react-native-fast-image";
+import ImageZoom from 'react-native-image-pan-zoom';
 
 export default class PhotoPage extends Component {
 
@@ -18,8 +20,10 @@ export default class PhotoPage extends Component {
         this.state = {
             loading: false,
             progress: 0,
-            hiddenStatusBar: false,
-            fadeAnimOpacity: new Animated.Value(0.5),
+            hiddenStatusBar: true,
+            fadeAnimOpacity: new Animated.Value(0),
+            width: 0,
+            height: 0,
         };
     }
 
@@ -28,15 +32,16 @@ export default class PhotoPage extends Component {
             this.state.fadeAnimOpacity,
             {
                 toValue: 1,
-                duration: 150,
+                duration: 250,
+                easing: Easing.out(Easing.cubic)
             }
         ).start(() => {
 
         });
 
-        this.setState({
-            hiddenStatusBar: true,
-        });
+        // this.setState({
+        //     hiddenStatusBar: true,
+        // });
 
         BackHandler.addEventListener('hardwareBackPress', () => {
             // this.onMainScreen and this.goBack are just examples, you need to use your own implementation here
@@ -56,29 +61,66 @@ export default class PhotoPage extends Component {
         this.setState({
             hiddenStatusBar: false,
         });
-        Animated.timing(
-            this.state.fadeAnimOpacity,
-            {
-                toValue: 0,
-                duration: 100,
-            }
-        ).start(() => {
-            this.props.onClosePress();
-        });
+        this.props.onClosePress();
+        // Animated.timing(
+        //     this.state.fadeAnimOpacity,
+        //     {
+        //         toValue: 0,
+        //         duration: 0,
+        //     }
+        // ).start(() => {
+        //     this.props.onClosePress();
+        // });
+    }
+
+    savePhoto() {
+
     }
 
     render() {
         return (
-            <Animated.View style={{position: 'absolute', top: 0,right: 0,bottom: 0,left: 0,backgroundColor: 'black',
-                opacity: this.state.fadeAnimOpacity}}>
+            <Animated.View
+                style={{
+                    position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, backgroundColor: 'black',
+                    opacity: this.state.fadeAnimOpacity
+                }}
+                onLayout={(event) => {
+                    let {x, y, width, height} = event.nativeEvent.layout;
+                    this.setState({
+                        width: width,
+                        height: height,
+                    })
+                }}
+            >
                 <StatusBar
                     hidden={this.state.hiddenStatusBar}
                     animated={true}
                     showHideTransition="slide"
                 />
-                <TPTouchable onPress={this.close.bind(this)}>
-                    <Text style={{backgroundColor:'green'}}>关闭</Text>
-                </TPTouchable>
+                {/*<TPTouchable onPress={this.close.bind(this)}>*/}
+                {/*<Text style={{backgroundColor:'green'}}>关闭-{this.state.progress}</Text>*/}
+                {/*</TPTouchable>*/}
+                <ImageZoom cropWidth={this.state.width}
+                           cropHeight={this.state.height}
+                           imageWidth={this.state.width}
+                           imageHeight={this.state.height}
+                           onClick={() => this.close()}
+                           doubleClickInterval={350}
+                           onLongPress={this.savePhoto.bind(this)}
+                >
+                    <FastImage
+                        style={{flex: 1, width: '100%', height: '100%'}}
+                        source={{
+                            uri: this.props.url,
+                        }}
+                        resizeMode={FastImage.resizeMode.contain}
+                        onProgress={(e) => {
+                            this.setState({
+                                progress: e.nativeEvent.loaded / e.nativeEvent.total
+                            })
+                        }}
+                    />
+                </ImageZoom>
             </Animated.View>
         )
     }
