86550a1de58681f8528a0f64933df0fb00159f53
li.zhaoguang <li.zhaoguang@immomo.com>
2018-02-11 19:05:29 +0800

    修复 iOS 启动问题

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import start from './js/App'
 import {Navigation, NativeEventsReceiver} from 'react-native-navigation';
-Navigation.isAppLaunched()
-    .then(appLaunched => {
-        if (appLaunched) {
-            start();
-        }
-        new NativeEventsReceiver().appLaunched(start);
-    });
+import {Platform} from "react-native";
+
+if (Platform.OS === 'android') {
+    Navigation.isAppLaunched()
+        .then(appLaunched => {
+            if (appLaunched) {
+                start();
+            }
+            new NativeEventsReceiver().appLaunched(start);
+        });
+} else {
+    start();
+}
+
