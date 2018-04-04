759d2cdaa88fdc0a2315ca4e8877586c3c2711ac
li.zhaoguang <li.zhaoguang@immomo.com>
2018-04-05 00:11:05 +0800

    尝试修复启动卡主的问题

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ import start from './js/App'
 import {Navigation, NativeEventsReceiver} from 'react-native-navigation';
 import {Platform} from "react-native";
 
-if (Platform.OS === 'android') {
-    Navigation.isAppLaunched()
-        .then(appLaunched => {
-            if (appLaunched) {
-                start();
-            }
-            new NativeEventsReceiver().appLaunched(start);
-        });
-} else {
-    start();
-}
+// if (Platform.OS === 'android') {
+//     Navigation.isAppLaunched()
+//         .then(appLaunched => {
+//             if (appLaunched) {
+//                 start();
+//             }
+//             new NativeEventsReceiver().appLaunched(start);
+//         });
+// } else {
+//     start();
+// }
 
+start();
